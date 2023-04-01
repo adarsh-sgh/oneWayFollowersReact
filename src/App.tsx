f5c4f3082111a30e68613b0f04ceac62b4a5cf17
Adarsh Singh <thakurluckysinghbrh@gmail.com>
2023-04-02 01:11:17 +0530

    add github button

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
-import Test from "./components/Test/Test";
-import Button from "./components/Button/button";
 import InputUsername from "./components/InputUsername/InputUsername";
 import InfoTable from "./components/InfoTable/InfoTable";
-
+import GitHubButton from "react-github-btn";
 function App() {
   const [onlyFollower, setOnlyFollower] = useState<any[]>([]);
   const [onlyFollowing, setOnlyFollowing] = useState<any>([]);
 
   return (
     <div className="App">
+      <div className="star">
+        <GitHubButton
+          href="https://github.com/adarsh-sgh/oneWayFollowersReact"
+          data-color-scheme="no-preference: dark_dimmed; light: light; dark: dark;"
+          data-icon="octicon-star"
+          data-size="large"
+          data-show-count="true"
+          aria-label="Star buttons/github-buttons on GitHub"
+        >
+          Star
+        </GitHubButton>
+      </div>
+
       <InputUsername
         setOnlyFollower={setOnlyFollower}
         setOnlyFollowing={setOnlyFollowing}
